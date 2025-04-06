9d85aba973a692343de28405d2756bf8769c084d
Drew Cook <dc333@usf.edu>
2025-04-06 15:06:20 -0400

    Task 1 and 2

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'; //import React to avoid errors in the console
 
-function Greeting() {
+function Greeting() { //create a function to greet
   return (
     <div>
       <h2>Hello from the Greeting Component!</h2>
       <p>This is your first custom component in React.</p>
-    </div>
+    </div> //wrap the greeting in a div tag as part of the rules for React
   );
 }
 
-export default Greeting;
+export default Greeting; //export the function to be used in other files
